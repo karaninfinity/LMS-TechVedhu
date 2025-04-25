50b657118361a650b6037930d1b6df5f2479c87f
keval-infinitysoftech <keval.infinitysoftech@gmail.com>
2025-04-25 18:30:52 +0530

    Update Changes

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import courseRoutes from "./routes/course.routes.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 
 // Get directory name in ES module
 const __filename = fileURLToPath(import.meta.url);
@@ -19,7 +20,6 @@ const __dirname = path.dirname(__filename);
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
