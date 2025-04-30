fba05ecb5340ccf689eb5d754d5db4da6dd5702e
karaninfinity <karan.infinitysoftech@gmail.com>
2025-04-30 12:25:22 +0530

    test for a lesson and course

---
+++ src/routes/lessonTest.routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  getTests,
+  getTest,
+  createTest,
+  updateTest,
+  deleteTest,
+  togglePublish,
+} from "../controllers/lessonTest.controller.js";
+import questionRoutes from "./question.routes.js";
+
+const router = express.Router({ mergeParams: true });
+
+router.use("/:testId/questions", questionRoutes);
+// Public routes
+router.get("/", getTests);
+router.get("/:id", getTest);
+router.post("/", createTest);
+router.put("/:id", updateTest);
+router.delete("/:id", deleteTest);
+router.patch("/:id/publish", togglePublish);
+
+export default router;
